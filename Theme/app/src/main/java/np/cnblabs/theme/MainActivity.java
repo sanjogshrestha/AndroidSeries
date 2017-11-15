e4b8387e9dc1eb083e2863431118c3973765017f
Sanjog Shrestha <Sanjogshrestha.nepal@gmail.com>
2017-11-15 14:59:56 +0545

    initial commit

---
+++ Theme/app/src/main/java/np/cnblabs/theme/MainActivity.java
@@ -0,0 +1,15 @@
+package np.cnblabs.theme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+* sanjogshrestha.nepal@gmail.com
+*/
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
