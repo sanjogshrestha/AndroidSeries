da52860b4302935874c35e1ee162e946da8d09c7
Sanjog Shrestha <Sanjogshrestha.nepal@gmail.com>
2018-03-05 15:57:40 +0545

    splash added

---
+++ Splash/app/src/main/java/np/cnblabs/splash/MainActivity.java
@@ -0,0 +1,18 @@
+package np.cnblabs.splash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sanjogstha on 3/5/18.
+ * sanjogshrestha.nepal@gmail.com
+ */
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Splash/app/src/main/java/np/cnblabs/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package np.cnblabs.splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
